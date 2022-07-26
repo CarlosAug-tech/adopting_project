3f1c1de8437cb57e346b9c6a717d3914d7411191
Carlos Augusto <carlosaugust.tech@gmail.com>
2022-07-26 09:12:45 -0300

    test: ensure field name is not provided and is return trhow Error

--- backend/tests/application/modules/accounts/usecases/create-user/create-user-usecase.spec.ts
+++ backend/tests/application/modules/accounts/usecases/create-user/create-user-usecase.spec.ts
@@ -1,6 +1,18 @@
 import { CreateUserUseCase } from '@application/modules/accounts/usecases/create-user/create-user-usecase';
 
 describe('Create User UseCase', () => {
+    it('should not be able to create a new User, if name is not provided', async () => {
+        const sut = new CreateUserUseCase();
+        const user = {
+            name: '',
+            email: 'any_email@email.com',
+            password: 'any_password',
+            confirmPassword: 'any_password',
+        };
+
+        await expect(sut.execute(user)).rejects.toThrow();
+    });
+
     it('should be able to create a new User', async () => {
         const sut = new CreateUserUseCase();
         const user = {
