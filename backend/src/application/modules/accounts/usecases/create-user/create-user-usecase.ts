897e8003214f423fad3470df6baa8119d7ebb66d
Carlos Augusto <carlosaugust.tech@gmail.com>
2022-07-26 08:52:56 -0300

    refactor: add dtos of request and response on create user usecase

--- backend/src/application/modules/accounts/usecases/create-user/create-user-usecase.ts
+++ backend/src/application/modules/accounts/usecases/create-user/create-user-usecase.ts
@@ -1,7 +1,21 @@
+import {
+    ICreateUserRequestDTO,
+    ICreateUserResponseDTO,
+} from '../../dtos/create-user-dtos';
+
+interface IRequest extends ICreateUserRequestDTO {
+    confirmPassword: string;
+}
+
 class CreateUserUseCase {
-    async execute(data: any): Promise<any> {
+    async execute(data: IRequest): Promise<ICreateUserResponseDTO> {
+        const { name, email, password, confirmPassword } = data;
+
         return {
             id: 'any_id',
+            name,
+            email,
+            created_at: new Date(),
         };
     }
 }
