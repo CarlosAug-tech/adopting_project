7f91928cde04ed362f7907b85b03d27c03be84e2
Carlos Augusto <carlosaugust.tech@gmail.com>
2022-07-27 08:49:28 -0300

    test: ensure password is match with method compare on bcrypt provider

--- backend/tests/application/modules/accounts/usecases/authentication/authentication-user-usecase.spec.ts
+++ backend/tests/application/modules/accounts/usecases/authentication/authentication-user-usecase.spec.ts
@@ -4,6 +4,7 @@ import {
 } from '@application/modules/accounts/dtos/create-user-dtos';
 import { IUsersRepository } from '@application/modules/accounts/repositories/users-repository';
 import { AuthenticationUserUseCase } from '@application/modules/accounts/usecases/authentication/authentication-user-usecase';
+import { IEncryptProvider } from '@application/providers/contracts/encrypt-provider';
 import { IUser } from '@domain/entities/user';
 
 const makeUsersRepositoryStub = (): IUsersRepository => {
@@ -28,10 +29,28 @@ const makeUsersRepositoryStub = (): IUsersRepository => {
     return new UsersRepositoryStub();
 };
 
+const makeBcryptProviderStub = (): IEncryptProvider => {
+    class BcryptProviderStub implements IEncryptProvider {
+        hash(password: string, hashSalt: number): Promise<string> {
+            throw new Error('Method not implemented.');
+        }
+
+        compare(password: string, password_hash: string): Promise<boolean> {
+            return new Promise(resolve => resolve(true));
+        }
+    }
+
+    return new BcryptProviderStub();
+};
+
 describe('Authentication User UseCase', () => {
     it('should not be able to authenticate a user, if email is not provided', async () => {
         const usersRepositoryStub = makeUsersRepositoryStub();
-        const sut = new AuthenticationUserUseCase(usersRepositoryStub);
+        const bcryptProviderStub = makeBcryptProviderStub();
+        const sut = new AuthenticationUserUseCase(
+            usersRepositoryStub,
+            bcryptProviderStub,
+        );
         const credentials = {
             email: '',
             password: 'any_password',
@@ -42,7 +61,11 @@ describe('Authentication User UseCase', () => {
 
     it('should not be able to authenticate a user, if password is not provided', async () => {
         const usersRepositoryStub = makeUsersRepositoryStub();
-        const sut = new AuthenticationUserUseCase(usersRepositoryStub);
+        const bcryptProviderStub = makeBcryptProviderStub();
+        const sut = new AuthenticationUserUseCase(
+            usersRepositoryStub,
+            bcryptProviderStub,
+        );
         const credentials = {
             email: 'any_email@email.com',
             password: '',
@@ -53,7 +76,11 @@ describe('Authentication User UseCase', () => {
 
     it('should not be able to authenticate user, if email is not exists', async () => {
         const usersRepositoryStub = makeUsersRepositoryStub();
-        const sut = new AuthenticationUserUseCase(usersRepositoryStub);
+        const bcryptProviderStub = makeBcryptProviderStub();
+        const sut = new AuthenticationUserUseCase(
+            usersRepositoryStub,
+            bcryptProviderStub,
+        );
         jest.spyOn(usersRepositoryStub, 'findByEmail').mockReturnValueOnce(
             undefined,
         );
@@ -65,9 +92,32 @@ describe('Authentication User UseCase', () => {
         await expect(sut.execute(credentials)).rejects.toThrow();
     });
 
+    it('should not be able to authenticate a user, if password is invalid', async () => {
+        const usersRepositoryStub = makeUsersRepositoryStub();
+        const bcryptProviderStub = makeBcryptProviderStub();
+        jest.spyOn(bcryptProviderStub, 'compare').mockReturnValueOnce(
+            new Promise(resolve => resolve(false)),
+        );
+        const sut = new AuthenticationUserUseCase(
+            usersRepositoryStub,
+            bcryptProviderStub,
+        );
+
+        const credentials = {
+            email: 'any_email@email.com',
+            password: 'any_password_invalid',
+        };
+
+        await expect(sut.execute(credentials)).rejects.toThrow();
+    });
+
     it('should be able to authenticate a user', async () => {
         const usersRepositoryStub = makeUsersRepositoryStub();
-        const sut = new AuthenticationUserUseCase(usersRepositoryStub);
+        const bcryptProviderStub = makeBcryptProviderStub();
+        const sut = new AuthenticationUserUseCase(
+            usersRepositoryStub,
+            bcryptProviderStub,
+        );
         const credentials = {
             email: 'any_email@email.com',
             password: 'any_password',
