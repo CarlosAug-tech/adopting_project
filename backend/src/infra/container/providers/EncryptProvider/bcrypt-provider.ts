ef7cdfd3ec6d4f86a6182118343de529327750c1
Carlos Augusto <carlosaugust.tech@gmail.com>
2022-07-27 08:35:53 -0300

    feat: add method compare on encrypt provider and also on bcrypt implementation

--- backend/src/infra/container/providers/EncryptProvider/bcrypt-provider.ts
+++ backend/src/infra/container/providers/EncryptProvider/bcrypt-provider.ts
@@ -1,11 +1,17 @@
 import { IEncryptProvider } from '@application/providers/contracts/encrypt-provider';
-import { hash } from 'bcrypt';
+import { compare, hash } from 'bcrypt';
 
 class BcryptProvider implements IEncryptProvider {
     async hash(password: string, hashSalt: number): Promise<string> {
         const passwordHash = await hash(password, hashSalt);
 
         return passwordHash;
+    }
+
+    async compare(password: string, password_hash: string): Promise<boolean> {
+        const passwordIsMatch = await compare(password, password_hash);
+
+        return passwordIsMatch;
     }
 }
 
