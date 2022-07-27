210efd38fbc87c4e8c03596dcfee1aa14fd32918
Carlos Augusto <carlosaugust.tech@gmail.com>
2022-07-27 08:03:01 -0300

    feat: add verification for fields requireds

--- backend/src/application/modules/accounts/usecases/authentication/authentication-user-usecase.ts
+++ backend/src/application/modules/accounts/usecases/authentication/authentication-user-usecase.ts
@@ -8,6 +8,13 @@ class AuthenticationUserUseCase {
         data: IAuthenticationUserRequestDTO,
     ): Promise<IAuthenticationUserResponseDTO> {
         const { email, password } = data;
+        const requiredFields = ['email', 'password'];
+
+        for (const field of requiredFields) {
+            if (!data[field]) {
+                throw new Error('This is field is required!');
+            }
+        }
 
         return {
             user: {
