8fd9426a394ebca4e0ea24676cfb436373b0c12c
Carlos Augusto <carlosaugust.tech@gmail.com>
2022-07-27 09:18:27 -0300

    feat: create authentication user controller and add on route session

--- backend/src/infra/http/routes/session.routes.ts
+++ backend/src/infra/http/routes/session.routes.ts
@@ -1,11 +1,9 @@
+import { AuthenticationUserController } from '@application/modules/accounts/usecases/authentication/authentication-user-controller';
 import { Router } from 'express';
 
 const sessionRoutes = Router();
+const authenticationUserController = new AuthenticationUserController();
 
-sessionRoutes.post('/sessions', (request, response) =>
-    response.status(200).json({
-        token: 'any_token',
-    }),
-);
+sessionRoutes.post('/sessions', authenticationUserController.handle);
 
 export { sessionRoutes };
