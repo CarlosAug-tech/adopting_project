7cd3a0fa72039de76f87e42b44b75b64f0db9f4e
Carlos Augusto <carlosaugust.tech@gmail.com>
2022-07-27 08:03:29 -0300

    test: ensure field email is provided

--- backend/tests/application/modules/accounts/usecases/authentication/authentication-user-usecase.spec.ts
+++ backend/tests/application/modules/accounts/usecases/authentication/authentication-user-usecase.spec.ts
@@ -1,6 +1,16 @@
 import { AuthenticationUserUseCase } from '@application/modules/accounts/usecases/authentication/authentication-user-usecase';
 
 describe('Authentication User UseCase', () => {
+    it('should not be able to authenticate a user, if email is not provided', async () => {
+        const sut = new AuthenticationUserUseCase();
+        const credentials = {
+            email: '',
+            password: 'any_password',
+        };
+
+        await expect(sut.execute(credentials)).rejects.toThrow();
+    });
+
     it('should be able to authenticate a user', async () => {
         const sut = new AuthenticationUserUseCase();
         const credentials = {
