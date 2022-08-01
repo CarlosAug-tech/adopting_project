9b8ebf3628fdc824bcba49d1aa498220cb3cd835
Carlos Augusto <carlosaugust.tech@gmail.com>
2022-08-01 11:10:06 -0300

    test: ensure field description is required for create animal

--- backend/tests/application/modules/animals/usecases/create-animal/create-animals-usecase.spec.ts
+++ backend/tests/application/modules/animals/usecases/create-animal/create-animals-usecase.spec.ts
@@ -16,4 +16,19 @@ describe('Create Animal UseCase', () => {
             new AppError('This field is required!'),
         );
     });
+
+    it('should not be able to create a animal, if description is not provided', async () => {
+        const sut = new CreateAnimalUseCase();
+        const animal = {
+            name: 'any_name',
+            description: '',
+            breed_id: 'any_id',
+            isPuppy: false,
+            isAdopt: false,
+        };
+
+        await expect(sut.execute(animal)).rejects.toEqual(
+            new AppError('This field is required!'),
+        );
+    });
 });
