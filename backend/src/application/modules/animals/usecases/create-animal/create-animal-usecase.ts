d5750e772da934553b8a499c875eeb1f010cc93e
Carlos Augusto <carlosaugust.tech@gmail.com>
2022-08-01 14:05:15 -0300

    feat: use method create of animals repository on create animal usecase

--- backend/src/application/modules/animals/usecases/create-animal/create-animal-usecase.ts
+++ backend/src/application/modules/animals/usecases/create-animal/create-animal-usecase.ts
@@ -1,15 +1,16 @@
 import { UseCase } from '@application/contracts/usecase';
+import { IAnimal } from '@domain/entities/animal';
 import { AppError } from '@infra/shared/utils/app-error';
 import { ICreateAnimalRequestDTO } from '../../dtos/create-animal-dtos';
 import { IAnimalsRepository } from '../../repositories/animals-repository';
 
-class CreateAnimalUseCase extends UseCase<ICreateAnimalRequestDTO> {
+class CreateAnimalUseCase extends UseCase<ICreateAnimalRequestDTO, IAnimal> {
     constructor(private animalsRepository: IAnimalsRepository) {
         super();
     }
 
-    async perform(data: ICreateAnimalRequestDTO): Promise<any> {
-        const { name, breed_id, isAdopt } = data;
+    async perform(data: ICreateAnimalRequestDTO): Promise<IAnimal> {
+        const { name, description, breed_id, sex, isPuppy, isAdopt } = data;
 
         if (isAdopt) {
             throw new AppError('Does not register a animal already adopting');
@@ -25,14 +26,25 @@ class CreateAnimalUseCase extends UseCase<ICreateAnimalRequestDTO> {
                 'This animal already register, change name please!',
             );
         }
+
+        const animal = await this.animalsRepository.create({
+            name,
+            description,
+            breed_id,
+            sex,
+            isPuppy,
+            isAdopt,
+        });
+
+        return animal;
     }
 
-    async execute(data: ICreateAnimalRequestDTO): Promise<any> {
+    async execute(data: ICreateAnimalRequestDTO): Promise<IAnimal> {
         const requiredFields = ['name', 'description', 'breed_id', 'sex'];
 
         this.validateRequiredFields(data, requiredFields);
 
-        await this.perform(data);
+        return this.perform(data);
     }
 }
 
