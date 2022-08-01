eaf18a0a153c552c231973a642804e68868ff1f0
Carlos Augusto <carlosaugust.tech@gmail.com>
2022-08-01 12:58:11 -0300

    refactor: adjustment usecase class abstract change method validateRequiredFields for protected and override method execute in create animal usecase

--- backend/src/application/modules/animals/usecases/create-animal/create-animal-usecase.ts
+++ backend/src/application/modules/animals/usecases/create-animal/create-animal-usecase.ts
@@ -1,8 +1,18 @@
 import { UseCase } from '@application/contracts/usecase';
 import { ICreateAnimalRequestDTO } from '../../dtos/create-animal-dtos';
 
-class CreateAnimalUseCase extends UseCase {
+class CreateAnimalUseCase extends UseCase<ICreateAnimalRequestDTO> {
+    constructor() {
+        super();
+    }
+
     async perform(data: ICreateAnimalRequestDTO): Promise<any> {}
+
+    async execute(data: ICreateAnimalRequestDTO): Promise<any> {
+        const requiredFields = ['name', 'description', 'breed_id'];
+
+        this.validateRequiredFields(data, requiredFields);
+    }
 }
 
 export { CreateAnimalUseCase };
