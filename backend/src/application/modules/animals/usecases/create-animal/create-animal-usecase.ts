07e9d28834b29872aa8d4ada14ca6141357405ed
Carlos Augusto <carlosaugust.tech@gmail.com>
2022-08-01 13:07:51 -0300

    feat: add verification wheter the animal already is adopting per someone

--- backend/src/application/modules/animals/usecases/create-animal/create-animal-usecase.ts
+++ backend/src/application/modules/animals/usecases/create-animal/create-animal-usecase.ts
@@ -1,4 +1,5 @@
 import { UseCase } from '@application/contracts/usecase';
+import { AppError } from '@infra/shared/utils/app-error';
 import { ICreateAnimalRequestDTO } from '../../dtos/create-animal-dtos';
 
 class CreateAnimalUseCase extends UseCase<ICreateAnimalRequestDTO> {
@@ -6,12 +7,20 @@ class CreateAnimalUseCase extends UseCase<ICreateAnimalRequestDTO> {
         super();
     }
 
-    async perform(data: ICreateAnimalRequestDTO): Promise<any> {}
+    async perform(data: ICreateAnimalRequestDTO): Promise<any> {
+        const { isAdopt } = data;
+
+        if (isAdopt) {
+            throw new AppError('Does not register a animal already adopting');
+        }
+    }
 
     async execute(data: ICreateAnimalRequestDTO): Promise<any> {
         const requiredFields = ['name', 'description', 'breed_id'];
 
         this.validateRequiredFields(data, requiredFields);
+
+        await this.perform(data);
     }
 }
 
