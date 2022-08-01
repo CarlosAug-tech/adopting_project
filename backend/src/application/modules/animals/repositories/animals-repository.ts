09a0ae09551610f4e97d2c74f3fcafbcd058c83e
Carlos Augusto <carlosaugust.tech@gmail.com>
2022-08-01 14:04:31 -0300

    feat: add method create on animals repository

--- backend/src/application/modules/animals/repositories/animals-repository.ts
+++ backend/src/application/modules/animals/repositories/animals-repository.ts
@@ -1,6 +1,8 @@
 import { IAnimal } from '@domain/entities/animal';
+import { ICreateAnimalRequestDTO } from '../dtos/create-animal-dtos';
 
 interface IAnimalsRepository {
+    create(data: ICreateAnimalRequestDTO): Promise<IAnimal>;
     findByNameAndBreed(name: string, breed_id: string): Promise<IAnimal>;
 }
 
