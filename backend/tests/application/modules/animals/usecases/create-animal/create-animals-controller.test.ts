666dfe7bbd8b5f8eecb6853c55c43864ca91f666
Carlos Augusto <carlosaugust.tech@gmail.com>
2022-08-10 14:02:21 -0300

    test: ensure field sex is not provided when create a new animal is return status of 400 and message This field is required!

--- backend/tests/application/modules/animals/usecases/create-animal/create-animals-controller.test.ts
+++ backend/tests/application/modules/animals/usecases/create-animal/create-animals-controller.test.ts
@@ -77,6 +77,26 @@ describe('Create animals controller', () => {
         expect(response.body.message).toEqual('This field is required!');
     });
 
+    it('should not be able to create a new animal, if sex is not provided', async () => {
+        const response = await request(app)
+            .post('/animals')
+            .send({
+                name: 'any_name',
+                description: 'any_description',
+                sex: '',
+                breed_id: idBreedGeneric,
+                type_id: idTypeGeneric,
+                isPuppy: true,
+                isAdopt: false,
+            })
+            .set({
+                Authorization: `Bearer ${token}`,
+            });
+
+        expect(response.status).toBe(400);
+        expect(response.body.message).toEqual('This field is required!');
+    });
+
     it('it should be able to create a new animal and return status 201(CREATED)', async () => {
         const response = await request(app)
             .post('/animals')
