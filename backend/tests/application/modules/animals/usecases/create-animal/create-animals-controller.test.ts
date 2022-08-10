d9d5fdb2760ef507ebcb8aee71404a42ec9a43cd
Carlos Augusto <carlosaugust.tech@gmail.com>
2022-08-10 13:59:03 -0300

    test: ensure field name is not provided when create a new animal is return status of 400 and message This field is required!

--- backend/tests/application/modules/animals/usecases/create-animal/create-animals-controller.test.ts
+++ backend/tests/application/modules/animals/usecases/create-animal/create-animals-controller.test.ts
@@ -37,6 +37,26 @@ describe('Create animals controller', () => {
         await connection.close();
     });
 
+    it('should not be able to create a new animal, if name is not provided', async () => {
+        const response = await request(app)
+            .post('/animals')
+            .send({
+                name: '',
+                description: 'any_description',
+                sex: 'any_sex',
+                breed_id: idBreedGeneric,
+                type_id: idTypeGeneric,
+                isPuppy: true,
+                isAdopt: false,
+            })
+            .set({
+                Authorization: `Bearer ${token}`,
+            });
+
+        expect(response.status).toBe(400);
+        expect(response.body.message).toEqual('This field is required!');
+    });
+
     it('it should be able to create a new animal and return status 201(CREATED)', async () => {
         const response = await request(app)
             .post('/animals')
