5ff5e829622e3a80cae6c67d87a9684467b5fa05
Carlos Augusto <carlosaugust.tech@gmail.com>
2022-08-10 13:49:27 -0300

    feat: add and create repository for breed and animal on typeorm

---
+++ backend/src/infra/database/typeorm/repositories/breeds-repository.ts
@@ -0,0 +1,20 @@
+import { IBreedsRepository } from '@application/modules/animals/repositories/breeds-repository';
+import { IBreed } from '@domain/entities/breed';
+import { getRepository, Repository } from 'typeorm';
+import { Breed } from '../entities/breed';
+
+class BreedsRepository implements IBreedsRepository {
+    private repository: Repository<Breed>;
+
+    constructor() {
+        this.repository = getRepository(Breed);
+    }
+
+    async findById(id: string): Promise<IBreed> {
+        const breed = await this.repository.findOne({ id });
+
+        return breed;
+    }
+}
+
+export { BreedsRepository };
