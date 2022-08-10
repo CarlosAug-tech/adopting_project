752c17363b5a9915e742e3ae646bbf1b81e07986
Carlos Augusto <carlosaugust.tech@gmail.com>
2022-08-10 13:51:56 -0300

    test: ensure create animals is return 201(CREATED)

---
+++ backend/tests/application/modules/animals/usecases/create-animal/create-animals-controller.test.ts
@@ -0,0 +1,59 @@
+import request from 'supertest';
+import { Connection } from 'typeorm';
+
+import { app } from '@infra/http/app';
+import createConnection from '@infra/database/typeorm';
+import { sign } from 'jsonwebtoken';
+import { v4 } from 'uuid';
+
+let connection: Connection;
+let token: string;
+let idBreedGeneric: string;
+let idTypeGeneric: string;
+
+describe('Create animals controller', () => {
+    beforeAll(async () => {
+        connection = await createConnection();
+        await connection.runMigrations();
+
+        idBreedGeneric = v4();
+        idTypeGeneric = v4();
+
+        await connection.query(`INSERT INTO BREEDS(id, name, description, created_at)
+        values('${idBreedGeneric}', 'any_name', 'any_description', 'now()')
+        `);
+
+        await connection.query(`INSERT INTO TYPES(id, name, created_at)
+        values('${idTypeGeneric}', 'any_name', 'now()')
+        `);
+
+        token = sign({}, 'any_token_secret', {
+            subject: 'any_id',
+        });
+    });
+
+    afterAll(async () => {
+        await connection.dropDatabase();
+        await connection.close();
+    });
+
+    it('it should be able to create a new animal and return status 201(CREATED)', async () => {
+        const response = await request(app)
+            .post('/animals')
+            .send({
+                name: 'any_name',
+                description: 'any_description',
+                sex: 'any_sex',
+                breed_id: idBreedGeneric,
+                type_id: idTypeGeneric,
+                isPuppy: true,
+                isAdopt: false,
+            })
+            .set({
+                Authorization: `Bearer ${token}`,
+            });
+
+        expect(response.status).toBe(201);
+        expect(response.body).toHaveProperty('id');
+    });
+});
