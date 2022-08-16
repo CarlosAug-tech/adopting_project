3453c782ce917c7e1c218a5aa713b2602b861e9d
Carlos Augusto <carlosaugust.tech@gmail.com>
2022-08-16 13:58:42 -0300

    feat: add method create in breeds repositories

--- backend/src/application/modules/animals/repositories/breeds-repository.ts
+++ backend/src/application/modules/animals/repositories/breeds-repository.ts
@@ -1,6 +1,8 @@
 import { IBreed } from '@domain/entities/breed';
+import { ICreateBreedRequestDTO } from '../dtos/create-breed-dtos';
 
 interface IBreedsRepository {
+    create(data: ICreateBreedRequestDTO): Promise<IBreed>;
     findById(id: string): Promise<IBreed>;
     findByName(name: string): Promise<IBreed>;
 }
--- backend/src/infra/database/typeorm/repositories/breeds-repository.ts
+++ backend/src/infra/database/typeorm/repositories/breeds-repository.ts
@@ -1,3 +1,4 @@
+import { ICreateBreedRequestDTO } from '@application/modules/animals/dtos/create-breed-dtos';
 import { IBreedsRepository } from '@application/modules/animals/repositories/breeds-repository';
 import { IBreed } from '@domain/entities/breed';
 import { getRepository, Repository } from 'typeorm';
@@ -8,6 +9,20 @@ class BreedsRepository implements IBreedsRepository {
 
     constructor() {
         this.repository = getRepository(Breed);
+    }
+
+    async create({
+        name,
+        description,
+    }: ICreateBreedRequestDTO): Promise<IBreed> {
+        const breed = this.repository.create({
+            name,
+            description,
+        });
+
+        await this.repository.save(breed);
+
+        return breed;
     }
 
     async findById(id: string): Promise<IBreed> {
