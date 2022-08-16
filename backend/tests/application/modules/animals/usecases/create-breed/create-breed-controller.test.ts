1f411989cfd450f488eead24337d2c44a3e467e3
Carlos Augusto <carlosaugust.tech@gmail.com>
2022-08-16 15:01:26 -0300

    test: ensure not create a new breed if already exists and return status 400 (BADREQUEST) and message error

--- backend/tests/application/modules/animals/usecases/create-breed/create-breed-controller.test.ts
+++ backend/tests/application/modules/animals/usecases/create-breed/create-breed-controller.test.ts
@@ -4,6 +4,7 @@ import { Connection } from 'typeorm';
 import createConnection from '@infra/database/typeorm';
 import { app } from '@infra/http/app';
 import { sign } from 'jsonwebtoken';
+import { v4 } from 'uuid';
 
 let connection: Connection;
 let token: string;
@@ -51,5 +52,24 @@ describe('Create Breed Controller', () => {
 
         expect(response.status).toBe(400);
         expect(response.body.message).toBe('This field is required!');
+    });
+
+    it('should not be able to create a new Breed, if breed already exists and return status 400 (BADREQUEST)', async () => {
+        await connection.query(`INSERT INTO BREEDS(id, name, description, created_at)
+        values('${v4()}', 'name_already_register', 'any_description', 'now()')
+        `);
+
+        const response = await request(app)
+            .post('/breeds')
+            .send({
+                name: 'name_already_register',
+                description: 'any_description',
+            })
+            .set({
+                Authorization: `Bearer ${token}`,
+            });
+
+        expect(response.status).toBe(400);
+        expect(response.body.message).toBe('This breed already exists');
     });
 });
