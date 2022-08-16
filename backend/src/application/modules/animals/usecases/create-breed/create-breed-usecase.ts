1a438d783577b716f6eb2bdc7fbf6e07e5505585
Carlos Augusto <carlosaugust.tech@gmail.com>
2022-08-16 13:41:09 -0300

    feat: add verification if breed already exists, when new breed is create

--- backend/src/application/modules/animals/usecases/create-breed/create-breed-usecase.ts
+++ backend/src/application/modules/animals/usecases/create-breed/create-breed-usecase.ts
@@ -1,8 +1,22 @@
 import { UseCase } from '@application/contracts/usecase';
+import { AppError } from '@infra/shared/utils/app-error';
 import { ICreateBreedRequestDTO } from '../../dtos/create-breed-dtos';
+import { IBreedsRepository } from '../../repositories/breeds-repository';
 
 class CreateBreedUseCase extends UseCase<ICreateBreedRequestDTO> {
+    constructor(private breedsRepository: IBreedsRepository) {
+        super();
+    }
+
     async perform(data?: ICreateBreedRequestDTO): Promise<any> {
+        const { name, description } = data;
+
+        const breedExists = await this.breedsRepository.findByName(name);
+
+        if (breedExists) {
+            throw new AppError('This breed already exists');
+        }
+
         return '';
     }
 }
