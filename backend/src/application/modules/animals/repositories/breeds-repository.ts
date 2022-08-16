bb0c0bf0eb96d1177452b4e6efeddb56e9b8dcfb
Carlos Augusto <carlosaugust.tech@gmail.com>
2022-08-16 13:39:27 -0300

    feat: add new method findByName on breeds repository

--- backend/src/application/modules/animals/repositories/breeds-repository.ts
+++ backend/src/application/modules/animals/repositories/breeds-repository.ts
@@ -2,6 +2,7 @@ import { IBreed } from '@domain/entities/breed';
 
 interface IBreedsRepository {
     findById(id: string): Promise<IBreed>;
+    findByName(name: string): Promise<IBreed>;
 }
 
 export { IBreedsRepository };
