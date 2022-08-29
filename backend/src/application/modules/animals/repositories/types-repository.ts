19e749ed4e391b2263cdec7b7fbd373dea99c115
Carlos Augusto <carlosaugust.tech@gmail.com>
2022-08-29 14:29:43 -0300

    feat: add method create on type repository

--- backend/src/application/modules/animals/repositories/types-repository.ts
+++ backend/src/application/modules/animals/repositories/types-repository.ts
@@ -1,7 +1,9 @@
 import { IType } from '@domain/entities/type';
+import { ICreateTypeRequestDTO } from '../dtos/create-type-dtos';
 
 interface ITypesRepository {
     findByName(name: string): Promise<IType>;
+    create(data: ICreateTypeRequestDTO): Promise<IType>;
 }
 
 export { ITypesRepository };
