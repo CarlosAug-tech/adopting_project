cb32dde895de782be3409da1121f90305a953c08
Carlos Augusto <carlosaugust.tech@gmail.com>
2022-08-29 15:25:17 -0300

    feat: add and create new route for create of type

--- backend/src/infra/http/routes/index.ts
+++ backend/src/infra/http/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { animalRoutes } from './animal.routes';
 import { breedRoutes } from './breed.routes';
 import { sessionRoutes } from './session.routes';
+import { typeRoutes } from './type.routes';
 import { userRoutes } from './user.routes';
 
 const routes = Router();
@@ -9,6 +10,7 @@ const routes = Router();
 routes.use('/users', userRoutes);
 routes.use('/animals', animalRoutes);
 routes.use('/breeds', breedRoutes);
+routes.use('/types', typeRoutes);
 routes.use(sessionRoutes);
 
 export { routes };
