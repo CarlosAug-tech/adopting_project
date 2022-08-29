7c98c1145b6b89d4c570036b6de6d6b2321e9e56
Carlos Augusto <carlosaugust.tech@gmail.com>
2022-08-29 14:19:46 -0300

    feat: check and verificate if type already register

--- backend/src/application/modules/animals/usecases/create-type/create-type-usecase.ts
+++ backend/src/application/modules/animals/usecases/create-type/create-type-usecase.ts
@@ -1,9 +1,21 @@
 import { UseCase } from '@application/contracts/usecase';
+import { AppError } from '@infra/shared/utils/app-error';
 import { ICreateTypeRequestDTO } from '../../dtos/create-type-dtos';
+import { ITypesRepository } from '../../repositories/types-repository';
 
 class CreateTypeUseCase extends UseCase<ICreateTypeRequestDTO> {
+    constructor(private typesRepository: ITypesRepository) {
+        super();
+    }
+
     async perform(data: ICreateTypeRequestDTO): Promise<any> {
         const { name } = data;
+
+        const typeExists = await this.typesRepository.findByName(name);
+
+        if (typeExists) {
+            throw new AppError('This type already register');
+        }
     }
 }
 
